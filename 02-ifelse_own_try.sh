59985528d93a50c9d5e048792bb65fe5626e8f5f
Vishwa <vishwa16108@gmail.com>
2024-03-14 21:04:34 +0530

    tyring

--- 02-ifelse_own_try.sh
+++ 02-ifelse_own_try.sh
@@ -5,9 +5,9 @@ NAME=$1
 if [ "$NAME" = "Vishwa" ] ; then
    echo -e $NAME, "\e[32m Working in T-Systems \e[0m"
 elif [ "$NAME" = "Danu" ] ; then
-   echo -e "\e[33m Working in Accenture \e[0m"
+   echo -e $NAME, "\e[33m Working in Accenture \e[0m"
 elif [ "$NAME" = "Atharv" ] ; then
-   echo -e "\e[34m Schooling in BALDWIN \e[0m"
+   echo -e $NAME, "\e[34m Schooling in BALDWIN \e[0m"
 else 
    echo -e "\e[35m Try typing for correct name \e[0m"
 
