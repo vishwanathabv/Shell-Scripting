74cabfbc5aedf5baa1c339b9b7f856c3f0ce9469
Vishwa <vishwa16108@gmail.com>
2024-03-14 20:27:58 +0530

    elseif statment trail

--- 02-ifelse.sh
+++ 02-ifelse.sh
@@ -4,9 +4,9 @@ ACTION=$1
 
 if [ "$ACTION" = "start" ] ; then
    echo -e "Starting dispatch service" 
-elif ["$ACTION" = "stop"] ; then
+elif [ "$ACTION" = "stop" ] ; then
    echo -e "Stopping the dispatch service"
-elif ["$ACTION" = "restart"] ; then
+elif [ "$ACTION" = "restart" ] ; then
    echo -e "Restarting the dispatch service" 
 else
    echo -e "Stopping the dispatch service"
