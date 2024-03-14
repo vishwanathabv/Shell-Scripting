a740e9110849bf2c86f3af5f6c16d444cb078b5c
Vishwa <vishwa16108@gmail.com>
2024-03-14 20:17:48 +0530

    starting scripting with Echo

--- 02-ifelse.sh
+++ 02-ifelse.sh
@@ -3,5 +3,9 @@
 ACTION=$1
 
 if [ "$ACTION" = "start" ] ; then
-   echo -e "Starting dispatch service"
+
+   echo -e "Starting dispatch service" 
+   else
+   echo -e "Stopping the dispatch service"
+
 fi
