63b4f2b7929a0ba032cc0440706f403bd55af2e8
Vishwa <vishwa16108@gmail.com>
2024-03-14 20:38:01 +0530

    trying to learn elseif conditions

---
+++ 02-ifelse_own_try.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAME=$1
+
+if [ "NAME" = "Vishwa"] ; then
+   echo -e "Working in T-Systems"
+elif [ "NAME" = "Danu"] ; then
+   echo -e "Working in Accenture"
+elif [ "NAME" = "Atharv"] ; then
+   echo -e "Schooling in BALDWIN"
+else 
+   echo -e "Try typing for correct name"
+
+fi
