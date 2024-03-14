4ce8bae2aa5bfe9e4cbefcda29f7a74065a51d8e
Vishwa <vishwa16108@gmail.com>
2024-03-14 20:46:15 +0530

    trying elseif

--- 02-ifelse_own_try.sh
+++ 02-ifelse_own_try.sh
@@ -3,12 +3,12 @@
 NAME=$1
 
 if [ "$NAME" = "Vishwa" ] ; then
-   echo -e "\e[32m Working in T-Systems\e[0m"
+   echo -e "\e[32m Working in T-Systems \e[0m"
 elif [ "$NAME" = "Danu" ] ; then
-   echo -e "Working in Accenture"
+   echo -e "\e[33m Working in Accenture \e[0m"
 elif [ "$NAME" = "Atharv" ] ; then
-   echo -e "Schooling in BALDWIN"
+   echo -e "\e[34m Schooling in BALDWIN \e[0m"
 else 
-   echo -e "Try typing for correct name"
+   echo -e "\e[35m Try typing for correct name \e[0m"
 
 fi
