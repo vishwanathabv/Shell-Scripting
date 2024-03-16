674e0596ee25f993756dbf26dec82adca23f5d01
Vishwa <vishwa16108@gmail.com>
2024-03-16 18:54:33 +0530

    bash practice

---
+++ forloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# #syntax for forloop:
+# for x in a b c d ; do
+# echo print $x   
+# done
+
+#Example of for loop:
+
+for x in 10 20 30 40 50 ; do
+   echo -e "values are $x"
+done
